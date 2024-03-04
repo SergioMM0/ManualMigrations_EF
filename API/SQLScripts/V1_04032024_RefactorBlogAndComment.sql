3432737c7b2e7ee2e5746bf24e354e6ad7484925
Sergio M <sergiomorenomartinez544@gmail.com>
2024-03-04 12:29:52 +0100

    Added SQL Script

---
+++ API/SQLScripts/V1_04032024_RefactorBlogAndComment.sql
@@ -0,0 +1,36 @@
+﻿BEGIN TRANSACTION;
+
+ALTER TABLE "Comments" ADD "Signature" TEXT NOT NULL DEFAULT '';
+
+CREATE TABLE "ef_temp_Blogs" (
+    "Id" TEXT NOT NULL CONSTRAINT "PK_Blogs" PRIMARY KEY,
+    "Content" TEXT NOT NULL,
+    "PriceOfSubscription" REAL NOT NULL,
+    "Title" TEXT NOT NULL
+);
+
+INSERT INTO "ef_temp_Blogs" ("Id", "Content", "PriceOfSubscription", "Title")
+SELECT "Id", "Content", "PriceOfSubscription", "Title"
+FROM "Blogs";
+
+COMMIT;
+
+PRAGMA foreign_keys = 0;
+
+BEGIN TRANSACTION;
+
+DROP TABLE "Blogs";
+
+ALTER TABLE "ef_temp_Blogs" RENAME TO "Blogs";
+
+COMMIT;
+
+PRAGMA foreign_keys = 1;
+
+BEGIN TRANSACTION;
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20240304095228_RefactorBlogAndComment', '7.0.16');
+
+COMMIT;
+
