4926b93dc1fbce615fe78f397611815985c5a940
Sergio M <sergiomorenomartinez544@gmail.com>
2024-03-04 10:20:19 +0100

    Updated db context

--- API/Infrastructure/DatabaseContext/AppDbContext.cs
+++ API/Infrastructure/DatabaseContext/AppDbContext.cs
@@ -1,7 +1,36 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using API.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Infrastructure.DatabaseContext {
     public class AppDbContext : DbContext {
+        public DbSet<Blog> Blogs { get; set; } = default!;
+        public DbSet<Comment> Comments { get; set; } = default!;
         
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+            modelBuilder.Entity<Blog>()
+                .HasMany(b => b.Comments)
+                .WithOne()
+                .HasForeignKey("BlogId")
+                .IsRequired();
+            
+            modelBuilder.Entity<Blog>()
+                .HasKey(b => b.Id);
+
+            modelBuilder.Entity<Comment>()
+                .HasKey(c => c.Id);
+            
+            modelBuilder.Entity<Comment>()
+                .Property(c => c.Text)
+                .IsRequired()
+                .HasMaxLength(500);
+        }
     }
 }
