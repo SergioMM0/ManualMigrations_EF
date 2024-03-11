f3180b0724a19940a1429702d93b75ce171efb0c
Sergio M <sergiomorenomartinez544@gmail.com>
2024-03-11 09:31:34 +0100

    Added Category_id to Products

--- API/Domain/Entities/Product.cs
+++ API/Domain/Entities/Product.cs
@@ -3,5 +3,6 @@ namespace API.Domain.Entities {
         public int Id { get; set; }
         public string Name { get; set; } = null!;
         public double Price { get; set; }
+        public int Category_Id { get; set; }
     }
 }
